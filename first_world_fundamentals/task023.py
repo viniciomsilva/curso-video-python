11362fa4107811bb0466e5f3fe2c9154ae2c1f9c
Vinicio Silva <viniciomsilva@outlook.com.br>
2025-10-02 20:36:30 -0300

    refactor task 023

--- first_world_fundamentals/task023.py
+++ first_world_fundamentals/task023.py
@@ -2,27 +2,26 @@
 # FAÇA UM PROGRAMA QUE LEIA UM NÚMERO ENTRE 0 E 9999 E MOSTRE CADA UM DOS
 # DÍGITOS SEPARADOS
 
-def divide(dividend: int, divisor: int) -> tuple[int, int]:
-    return (dividend // divisor), (dividend % divisor)
+
+decimal_orders = [
+    "UNIDADE", "DEZENA", "CENTENA",
+    "UNIDADE DE MILHAR", "DEZENA DE MILHAR", "CENTENA DE MILHAR"
+]
+
+
+def print_separate_numbers(num):
+    length = len(num)
+    num = int(num)
+    divisor = 1
+
+    print()
+    for i in range(length):
+        print(f"{decimal_orders[i]}: {num // divisor % 10}")
+
+        divisor *= 10
 
 
 if __name__ == "__main__":
-    num = input("DIGITE UM NÚMERO ENTRE 0 E 9999: ").rjust(4, "0")
+    num = input("DIGITE UM NÚMERO ENTRE 0 E 999999: ")
 
-    print("\nCOM MANIPULAÇÃO DE TEXTO")
-    print(f"MILHAR................: {num[0]}")
-    print(f"CENTENA...............: {num[1]}")
-    print(f"DEZENA................: {num[2]}")
-    print(f"UNIDADE...............: {num[3]}")
-
-    num = int(num)
-
-    m, rest = divide(num, 1000)
-    c, rest = divide(rest, 100)
-    d, rest = divide(rest, 10)
-
-    print("\nCOM MANIPULAÇÃO NUMÉRICA")
-    print(f"MILHAR................: {m}")
-    print(f"CENTENA...............: {c}")
-    print(f"DEZENA................: {d}")
-    print(f"UNIDADE...............: {rest}")
+    print_separate_numbers(num=num)
