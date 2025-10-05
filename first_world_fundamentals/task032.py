e9374db0f6f7d92388837547b2afbde9c5f0f3b2
Vinicio Silva <viniciomsilva@outlook.com.br>
2025-10-05 19:14:42 -0300

    do task032

---
+++ first_world_fundamentals/task032.py
@@ -0,0 +1,18 @@
+# 032
+# FAÇA UM PROGRAMA QUE LEIA UM ANO QUALQUER E MOSTRE SE ELE É BISSEXTO.
+
+# REGRA DOS BISSEXTOS
+# PARA ANOS CENTENÁRIOS
+#   DEVE-SE SER DIVISÍVEL POR 400
+# PARA ANOS NORMAIS
+#   DEVE-SE SER DIVISÍVEL POR 4
+
+
+if __name__ == "__main__":
+    year = int(input("DIGITE UM ANO QUALQUER (YYYY): "))
+
+    print(
+        "ESSE É UM ANO BISSEXTO"
+        if year % 4 == 0 or year % 400 == 0
+        else "ESSE NÃO É UM ANO BISSEXTO"
+    )
