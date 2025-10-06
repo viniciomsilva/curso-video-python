676e16e58defb1c7fca9d7b1040ac8c375f689b9
Vinicio Silva <viniciomsilva@outlook.com.br>
2025-10-06 05:00:49 -0300

    add feature to get pc year

--- first_world_fundamentals/task032.py
+++ first_world_fundamentals/task032.py
@@ -7,12 +7,15 @@
 # PARA ANOS NORMAIS
 #   DEVE-SE SER DIVISÍVEL POR 4
 
+from datetime import date
 
 if __name__ == "__main__":
     year = int(input("DIGITE UM ANO QUALQUER (YYYY): "))
 
+    year = year if year >= 0 else date.today().year
+
     print(
-        "ESSE É UM ANO BISSEXTO"
+        f"{year} É UM ANO BISSEXTO"
         if year % 4 == 0 and year % 100 != 0 or year % 400 == 0
-        else "ESSE NÃO É UM ANO BISSEXTO"
+        else f"{year} NÃO É UM ANO BISSEXTO"
     )
