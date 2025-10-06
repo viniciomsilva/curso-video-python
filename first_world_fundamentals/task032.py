1e7c449198c6127d07f84409fb657c5bb5ac9bd9
Vinicio Silva <viniciomsilva@outlook.com.br>
2025-10-06 04:53:48 -0300

    correct logic

--- first_world_fundamentals/task032.py
+++ first_world_fundamentals/task032.py
@@ -13,6 +13,6 @@ if __name__ == "__main__":
 
     print(
         "ESSE É UM ANO BISSEXTO"
-        if year % 4 == 0 or year % 400 == 0
+        if year % 4 == 0 and year % 100 != 0 or year % 400 == 0
         else "ESSE NÃO É UM ANO BISSEXTO"
     )
