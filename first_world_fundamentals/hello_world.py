57e6177139d9f46acf5c572925a3091cbfef17aa
viniciomsilva <viniciomsilva@proton.me>
2025-10-07 22:11:00 -0300

    customize the hello world

--- first_world_fundamentals/hello_world.py
+++ first_world_fundamentals/hello_world.py
@@ -1,1 +1,8 @@
-print("HELLO, WORLD!")
+from custom import customize
+
+if __name__ == "__main__":
+    print(
+        "🌎 {}Hello World!".format(
+            customize(style="bold", color="green"),
+        )
+    )
