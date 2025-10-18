1f4f4ab8bea32a6368b2fbdda815d5845a8ae7ce
viniciomsilva <viniciomsilva@proton.me>
2025-10-18 15:24:14 -0300

    Add delimiter parameter in read_csv function

--- scripts/database.py
+++ scripts/database.py
@@ -5,6 +5,6 @@ from pathlib import Path
 DATA_PATH = Path.joinpath(Path(__file__).parent.parent, "data").resolve()
 
 
-def read_csv(filename: str) -> list:
+def read_csv(filename: str, delimiter: str = ",") -> list:
     with open(Path.joinpath(DATA_PATH, filename).resolve()) as file:
-        return list(reader(file))
+        return list(reader(file, delimiter))
