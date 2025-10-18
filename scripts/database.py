997bff5abf28c97b2228c8a36d3b4c76451a5421
viniciomsilva <viniciomsilva@proton.me>
2025-10-18 18:16:06 -0300

    Fix dialect in read_csv function

--- scripts/database.py
+++ scripts/database.py
@@ -6,5 +6,14 @@ DATA_PATH = Path.joinpath(Path(__file__).parent.parent, "data").resolve()
 
 
 def read_csv(filename: str, delimiter: str = ",") -> list:
-    with open(Path.joinpath(DATA_PATH, filename).resolve()) as file:
-        return list(reader(file, delimiter))
+    with open(
+        Path.joinpath(DATA_PATH, filename).resolve(),
+        "r",
+    ) as file:
+        return list(
+            reader(
+                file,
+                dialect="excel",
+                delimiter=delimiter,
+            )
+        )
